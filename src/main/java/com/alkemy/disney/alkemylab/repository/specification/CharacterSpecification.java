175f241da8e501a287d5aea46b18a24a63aa19dc
Beniko19 <martin192012@gmail.com>
2022-06-27 21:09:01 -0300

    fixed the specification of movie and character to look up for filters.

--- src/main/java/com/alkemy/disney/alkemylab/repository/specification/CharacterSpecification.java
+++ src/main/java/com/alkemy/disney/alkemylab/repository/specification/CharacterSpecification.java
@@ -1,9 +1,12 @@
 package com.alkemy.disney.alkemylab.repository.specification;
 
-import com.alkemy.disney.alkemylab.dto.CharacterFiltersDTO;
+import com.alkemy.disney.alkemylab.dto.character.CharacterFiltersDTO;
 import com.alkemy.disney.alkemylab.entity.CharacterEntity;
+import com.alkemy.disney.alkemylab.entity.MovieEntity;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.criteria.*;
 import java.util.ArrayList;
@@ -13,20 +16,23 @@ public class CharacterSpecification {
     public Specification<CharacterEntity> getByFilters(CharacterFiltersDTO filtersDTO) {
         return ((root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
-            if (filtersDTO.getName()!= null) {
-                predicates.add(criteriaBuilder.equal(root.get("name"), filtersDTO.getName()));
+
+            if (StringUtils.hasLength(filtersDTO.getName())){
+                predicates.add(
+                        criteriaBuilder.like(
+                                criteriaBuilder.lower(root.get("name")),
+                                "%" + filtersDTO.getName().toLowerCase() + "%"));
             }
-            if (filtersDTO.getAge() != null ) {
-                predicates.add(criteriaBuilder.equal(root.get("age"),filtersDTO.getAge()));
-            }
-            if (filtersDTO.getWeight() != null ) {
+            if (filtersDTO.getWeight() != null)
                 predicates.add(criteriaBuilder.equal(root.get("weight"), filtersDTO.getWeight()));
-            }
-            /*if (filtersDTO.getMovies() != null && !CollectionUtils.isEmpty(filtersDTO.getMovies())) {
+            if (filtersDTO.getAge() != null)
+                predicates.add(criteriaBuilder.equal(root.get("age"), filtersDTO.getAge()));
+
+            if (!CollectionUtils.isEmpty(filtersDTO.getMovies())) {
                 Join<MovieEntity, CharacterEntity> join = root.join("movies", JoinType.INNER);
                 Expression<String> moviesId = join.get("id");
                 predicates.add(moviesId.in(filtersDTO.getMovies()));
-            }*/
+            }
 
             // Remove duplicates
             query.distinct(true);
@@ -40,8 +46,6 @@ public class CharacterSpecification {
                                 criteriaBuilder.desc(root.get(orderByField))
                 );
             }
-
-
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         });
     }
